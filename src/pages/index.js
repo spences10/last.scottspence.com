d06f15f4a33852a5759e8dec145a530f7b54b43d
spences10 <spences10apps@gmail.com>
2020-05-20 18:56:20 +0100

    add content to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import About from '../../content/copy/about'
+import Now from '../../content/copy/now'
+import Portfolio from '../../content/copy/portfolio'
+import Uses from '../../content/copy/uses'
 
 export default () => {
   return (
     <>
       <Link to="/garden">Garden</Link>
       <Link to="/tags">Tags</Link>
+      <About />
+      <Portfolio />
+      <Now />
+      <Uses />
     </>
   )
 }
