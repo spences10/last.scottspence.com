1d724d690851675a314ab9cee67958720354c6ca
spences10 <spences10apps@gmail.com>
2020-05-21 20:40:14 +0100

    tidy tags

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,6 +1,26 @@
 import { graphql, Link } from 'gatsby'
 import React from 'react'
+import styled from 'styled-components'
 import { H2, Li, Ul } from '../components/page-elements'
+
+const StyledTags = styled.article`
+  ul {
+    display: flex;
+    justify-content: start;
+    flex-flow: row wrap;
+    padding: 0rem;
+    margin: 0rem;
+  }
+  li {
+    padding-top: 0.75rem;
+    padding-bottom: 0.75rem;
+    padding-left: 0.25rem;
+    padding-right: 0.25rem;
+    list-style-type: none;
+    margin: 1rem;
+    margin-left: 0;
+  }
+`
 
 export default ({
   data: {
@@ -9,15 +29,19 @@ export default ({
 }) => (
   <>
     <H2>Tags</H2>
-    <Ul>
-      {group.map(({ fieldValue, totalCount }) => (
-        <Li key={fieldValue}>
-          <Link to={`/tags/${fieldValue}/`}>
-            {fieldValue} ({totalCount})
-          </Link>
-        </Li>
-      ))}
-    </Ul>
+    <StyledTags>
+      <Ul>
+        {group.map(({ fieldValue, totalCount }) => {
+          return (
+            <Li key={fieldValue}>
+              <Link to={`/tags/${fieldValue}/`}>
+                {fieldValue} ({totalCount})
+              </Link>
+            </Li>
+          )
+        })}
+      </Ul>
+    </StyledTags>
   </>
 )
 
