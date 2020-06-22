d665b66e8f9aa732e255d3c6fc9fdc2c6f978b4e
spences10 <spences10apps@gmail.com>
2020-06-22 18:00:42 +0100

    too tight line height

--- src/components/page-elements/h1.js
+++ src/components/page-elements/h1.js
@@ -6,7 +6,7 @@ const StyledText = styled.h1`
   font-size: ${({ theme }) => theme.fontSize['4xl']};
   font-family: ${({ theme }) => theme.fontFamily.serif};
   margin-top: ${({ theme }) => theme.spacing[8]};
-  line-height: ${({ theme }) => theme.lineHeight.none};
+  line-height: ${({ theme }) => theme.lineHeight.tight};
   ${AutoLink};
 `
 
