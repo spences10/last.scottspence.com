60526c6a01471e96602dceb519c3687eac9dd831
spences10 <spences10apps@gmail.com>
2020-07-08 15:55:53 +0100

    remove unused packages, revert gatsby-react-router-scroll

--- src/root-wrapper.js
+++ src/root-wrapper.js
@@ -65,14 +65,6 @@ const components = {
   ConvertKitForm,
 }
 
-if (typeof window !== 'undefined') {
-  require('smooth-scroll')('a[href*="#"]', {
-    speed: 500,
-    speedAsDuration: true,
-    easing: 'easeInOutCubic',
-  })
-}
-
 export const wrapPageElement = ({ element }) => (
   <>
     <Helmet>
