7f15b5a994fe93c6423baf56f828268ed20c494a
spences10 <spences10apps@gmail.com>
2020-08-06 20:33:40 +0100

    add window size hook

--- qawolf.config.js
+++ qawolf.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   config: 'node_modules/qawolf/js-jest.config.json',
   rootDir: '.qawolf',
-  testTimeout: 5000,
+  testTimeout: 60000,
   useTypeScript: false,
 }
+
+// window.innerHeight = 720
+// window.innerWidth = 1280
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react'
 import SEO from 'react-seo-component'
-import { down } from 'styled-breakpoints'
 import styled from 'styled-components'
 import About from '../../content/copy/about'
 import Now from '../../content/copy/now'
@@ -11,65 +10,37 @@ import { MugFace } from '../components/mug-face'
 import { NavItems } from '../components/nav-items'
 import { useOnScreen } from '../hooks/use-on-screen'
 import { useSiteMetadata } from '../hooks/use-site-metadata'
+import { useWindowSize } from '../hooks/use-window-size'
 import { ogImageUrl } from '../util/build-og-image-url'
 
 const Wrapper = styled.section`
-  position: relative;
-  display: grid;
-  grid-auto-rows: min-content;
   min-height: 90vh;
-  ${down('md')} {
-    min-height: 80vh;
-  }
-  div {
+  .gatsby-image-wrapper {
     margin: 0 auto;
-    margin-top: 20%;
     border-radius: ${({ theme }) => theme.borderRadius.full};
     height: 200px;
     width: 200px;
-    ${down('sm')} {
-      height: 150px;
-      width: 150px;
-    }
   }
   h1 {
     text-align: center;
-    margin-top: 20%;
-    ${down('sm')} {
-      margin-top: 10%;
-    }
-    span {
-      &:before {
-        content: ' ';
-      }
-    }
   }
   nav {
     position: relative;
+    display: grid;
+    grid-gap: 30px;
     bottom: 0;
     margin: 0 -10rem;
     margin-top: 25%;
-    display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: auto;
     grid-template-areas: 'about portfolio now uses';
-    ${down('md')} {
-      grid-template-columns: repeat(2, 1fr);
-      grid-template-areas:
-        'about portfolio'
-        'now uses';
-      margin: 0;
-      margin-top: 30%;
-    }
-    ${down('sm')} {
-      margin-top: 20%;
-    }
-    grid-gap: 30px;
   }
 `
 
 const LandingPage = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>
+  const { height } = useWindowSize()
+
+  return <Wrapper height={height}>{children}</Wrapper>
 }
 
 export default () => {
@@ -84,6 +55,7 @@ export default () => {
   } = useSiteMetadata()
   const ref = useRef()
   const onScreen = useOnScreen(ref)
+
   return (
     <>
       <SEO
