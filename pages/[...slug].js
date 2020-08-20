c682ae10fd0bb84629e4d0bf9cea9bfb5daeb354
spences10 <spences10apps@gmail.com>
2020-08-20 19:29:29 +0100

    get mdx rendering with [...slug].js 🎉

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,39 +1,93 @@
-// import hydrate from 'next-mdx-remote/hydrate'
+import fs from 'fs'
+import matter from 'gray-matter'
+import hydrate from 'next-mdx-remote/hydrate'
+import renderToString from 'next-mdx-remote/render-to-string'
+import Code from '../components/code'
 import Layout from '../components/layout'
-import { getPostPaths } from '../lib/posts'
+import { getPostPaths, getPostSlugs } from '../lib/posts'
 
-export default function Post({ mdxSource, frontMatter, slug }) {
-  console.log('=====================')
-  // console.log(`source::: ${mdxSource}`)
-  // console.log(`frontmatter::: ${frontMatter}`)
-  console.log(`slug::: ${slug}`)
-  console.log('=====================')
-  return <Layout>yo</Layout>
+const components = { code: Code }
+
+export default function Post({ mdxSource, frontMatter }) {
+  const content = hydrate(mdxSource, components)
+  return (
+    <Layout>
+      <h1>{frontMatter.title}</h1>
+      {content}
+    </Layout>
+  )
 }
 
 export const getStaticPaths = async () => {
-  const files = await getPostPaths()
-
-  const paths = files.map(path => {
-    const split = path.split(`/`)
-    // remove `content/posts` and `index.md*`
-    const removedIndexFile = split.splice(2, split.length - 3)
-
-    return {
-      params: {
-        slug: removedIndexFile,
-      },
-    }
-  })
-
-  return {
-    paths,
-    fallback: false,
-  }
+  return await getPostSlugs()
 }
 
 export const getStaticProps = async ({ params: { slug } }) => {
+  // match the slug and the file path
+  const files = getPostPaths(true)
+  const matchedPath = (await files).filter(path => {
+    return path.includes(slug.join(`/`))
+  })
+  const mdxSource = fs.readFileSync(matchedPath.toString())
+  const { content, data } = matter(mdxSource)
+
+  if (!matchedPath) {
+    console.warn('No MDX file found for slug')
+  }
+
+  const mdx = await renderToString(content, components, null, data)
+
   return {
-    props: { slug },
+    props: {
+      mdxSource: mdx,
+      frontMatter: data,
+    },
   }
 }
+
+// const postsDirectory = './content/posts'
+
+// export const getStaticPaths = async () => {
+//   const slugs = await getPostSlugs()
+//   const paths = [
+//     {
+//       params: {
+//         slug: '/private/convert-gatsby-starter-blog-to-mdx-part-two',
+//       },
+//     },
+//   ]
+//   return {
+//     paths,
+//     fallback: false,
+//   }
+// }
+
+// export const getStaticProps = async ({ params: { slug } }) => {
+//   console.log('=====================')
+//   console.log(`Slllllllllllllllluuuggg:: ${slug}`)
+//   console.log('=====================')
+//   // const files = glob.sync(contentGlob)
+
+//   // const fullPath = files.filter(item => {
+//   //   const split = item.split('/')
+//   //   const filename = split[split.length - 1]
+//   //   return filename.replace('.mdx', '') === slug
+//   // })[0]
+
+//   // const mdxSource = fs.readFileSync(fullPath)
+//   // const { content, data } = matter(mdxSource)
+
+//   // if (!fullPath) {
+//   //   console.warn('No MDX file found for slug')
+//   // }
+
+//   // const mdx = await renderToString(content, components, null, data)
+
+//   return {
+//     props: {
+//       slug,
+//       // mdxSource: mdx,
+//       // frontMatter: data,
+//     },
+//   }
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
 export default function Index(params) {
   return <h1>Yo</h1>
 }
+
+export const getServerSideProps = async () => {
+  // console.log('=====================')
+  // console.log(`SLUGS :::`, await getPostSlugs())
+  // console.log('=====================')
+  // console.log('=====================')
+  // console.log(`PATHS :::`, await getPostPaths())
+  // console.log('=====================')
+  return { props: {} }
+}
