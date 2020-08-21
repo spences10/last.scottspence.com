feac8aedf42a8dec61b35c196c0f88e027dc472c
spences10 <spences10apps@gmail.com>
2020-08-21 15:50:48 +0100

    Content links

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,54 @@
-export default function Index(params) {
-  return <h1>Yo</h1>
+import Link from 'next/link'
+import { getPostSlugs } from '../lib/posts'
+
+const Dump = props => (
+  <div
+    style={{
+      fontSize: 20,
+      border: '1px solid #efefef',
+      padding: 10,
+      background: 'white',
+    }}
+  >
+    {Object.entries(props).map(([key, val]) => (
+      <pre key={key}>
+        <strong style={{ color: 'white', background: 'red' }}>
+          {key} 💩
+        </strong>
+        {JSON.stringify(val, '', ' ')}
+      </pre>
+    ))}
+  </div>
+)
+
+export default function Index({ postSlugs }) {
+  const { paths } = postSlugs
+  return (
+    <>
+      <h1>Yo</h1>
+      <ul>
+        {paths.map(({ params: { slug } }) => {
+          const path = slug.join(`/`)
+          {
+            return (
+              <li key={path}>
+                <Link href="/[slug]" as={`/${path}`}>
+                  <a>{path}</a>
+                </Link>
+              </li>
+            )
+          }
+        })}
+      </ul>
+    </>
+  )
 }
 
-export const getServerSideProps = async () => {
-  // console.log('=====================')
-  // console.log(`SLUGS :::`, await getPostSlugs())
-  // console.log('=====================')
-  // console.log('=====================')
-  // console.log(`PATHS :::`, await getPostPaths())
-  // console.log('=====================')
-  return { props: {} }
+export const getStaticProps = async () => {
+  const postSlugs = await getPostSlugs()
+  return {
+    props: {
+      postSlugs,
+    },
+  }
 }
