3406e4958a9c471bbfff9d1178fc5a011f153f15
spences10 <spences10apps@gmail.com>
2020-09-19 17:57:15 +0100

    add theme vars

--- src/components/markdown-parser.js
+++ src/components/markdown-parser.js
@@ -1,23 +1,54 @@
 import Markdown from 'markdown-to-jsx'
 import React from 'react'
 import styled, { css } from 'styled-components'
+import useDarkMode from 'use-dark-mode'
+import { negMargin } from './shared-styles'
 
 const sharedCss = css`
   flex: 0 0 50%;
   padding: 1em;
 `
 
-const Demo = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-grow: 1;
-  margin-left: -1.5rem;
-  margin-right: -1.5rem;
-  @media all and (min-width: 1024px) {
-    margin-left: 0;
-    margin-right: 0;
+  margin: 2rem 0;
+  ${negMargin};
+  h1 {
+    font-size: 2.5rem;
   }
-  @media all and (max-width: 500px) {
-    flex-direction: column;
+  h2 {
+    font-size: 2rem;
+  }
+  h3 {
+    font-size: 1.75rem;
+  }
+  h4 {
+    font-size: 1.5rem;
+  }
+  h5 {
+    font-size: 1.25rem;
+  }
+  h6 {
+    font-size: 1.2rem;
+  }
+  p {
+    em {
+      font-weight: bold;
+    }
+    font-size: 1rem;
+  }
+  ul {
+    list-style-type: disc;
+    li {
+      font-size: 1rem;
+    }
+  }
+  ol {
+    list-style-type: decimal;
+    li {
+      font-size: 1rem;
+    }
   }
 `
 
@@ -27,7 +58,10 @@ const Compiled = styled.div`
   padding-right: 1rem;
   overflow: auto;
   overflow-x: hidden;
-  background-color: black;
+  background-color: var(
+    --colour-background-two,
+    ${({ theme }) => theme.colors.gray[200]}
+  );
 `
 
 const Textarea = styled.textarea`
@@ -35,29 +69,42 @@ const Textarea = styled.textarea`
   border: 0;
   color: inherit;
   position: sticky;
+  background-color: var(
+    --colour-background-two,
+    ${({ theme }) => theme.colors.gray[200]}
+  );
   top: 0;
   font-family: 'Source Code Pro', Consolas, Monaco, monospace;
   font-size: inherit;
   max-height: 100vh;
-  @media all and (max-width: 500px) {
-    height: 300px;
-    position: relative;
-  }
 `
 
-export const MarkdownParser = () => {
-  const [markdown, setMarkdown] = React.useState(`# This is Markdown`)
+const markdownExample = `
+# This is a heading
+
+This is a paragraph
+
+_bold lettering_
+
+~~Strikethrough~~
+`
+
+export const MarkdownParser = ({ markdownContent }) => {
+  const [markdown, setMarkdown] = React.useState(
+    !markdownContent ? markdownExample : markdownContent
+  )
 
   const handleInput = React.useCallback(
     e => setMarkdown(e.target.value),
     []
   )
+  const { value } = useDarkMode()
   return (
-    <Demo>
+    <Wrapper>
       <Textarea onInput={handleInput} value={markdown} />
       <Compiled>
         <Markdown>{markdown}</Markdown>
       </Compiled>
-    </Demo>
+    </Wrapper>
   )
 }
