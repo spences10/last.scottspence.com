f9329f5c5f5824bbab84b8a299169262d368cf49
spences10 <spences10apps@gmail.com>
2020-10-27 19:18:13 +0000

    add ssg's

--- src/components/page-elements/p.js
+++ src/components/page-elements/p.js
@@ -6,7 +6,7 @@ const StyledText = styled.p`
   font-size: ${({ theme }) => theme.fontSize.lg};
   margin-top: ${({ theme }) => theme.spacing[6]};
   strong {
-    font-weight: 500;
+    font-weight: 700;
   }
   em {
     font-style: italic;
