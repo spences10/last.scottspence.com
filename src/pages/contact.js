64adbdd246a8853aee6798b1e14683ff535f3aaf
spences10 <spences10apps@gmail.com>
2020-12-12 08:33:20 +0000

    remove sc

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,51 +1,51 @@
-import kwesforms from 'kwesforms'
+// import kwesforms from 'kwesforms'
 import React, { useEffect } from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-const Wrapper = styled.main`
-  min-height: 60vh;
-  form {
-    margin: 0 auto;
-    display: grid;
-    max-width: 70%;
-    input,
-    textarea,
-    button {
-      padding: 0.2rem 0.2rem;
-      border-radius: ${({ theme }) => theme.borderRadius.lg};
-      border: 0;
-      font-family: inherit;
-      font-size: inherit;
-    }
-    label {
-      margin-top: 1.5rem;
-    }
-    input {
-      font-weight: ${({ theme }) => theme.fontWeight.semibold};
-    }
-    textarea {
-      height: 100px;
-    }
-    button {
-      margin-top: 3rem;
-      font-weight: ${({ theme }) => theme.fontWeight.semibold};
-      color: ${({ theme }) => theme.colors.gray[100]};
-      background: linear-gradient(
-        180turn,
-        var(
-          --title-gradient-from,
-          ${({ theme }) => theme.colors.primary[200]}
-        ),
-        var(
-          --title-gradient-to,
-          ${({ theme }) => theme.colors.primary[500]}
-        )
-      );
-      padding: 0.65rem 1rem;
-      text-align: center;
-    }
-  }
-`
+// const Wrapper = styled.main`
+//   min-height: 60vh;
+//   form {
+//     margin: 0 auto;
+//     display: grid;
+//     max-width: 70%;
+//     input,
+//     textarea,
+//     button {
+//       padding: 0.2rem 0.2rem;
+//       border-radius: ${({ theme }) => theme.borderRadius.lg};
+//       border: 0;
+//       font-family: inherit;
+//       font-size: inherit;
+//     }
+//     label {
+//       margin-top: 1.5rem;
+//     }
+//     input {
+//       font-weight: ${({ theme }) => theme.fontWeight.semibold};
+//     }
+//     textarea {
+//       height: 100px;
+//     }
+//     button {
+//       margin-top: 3rem;
+//       font-weight: ${({ theme }) => theme.fontWeight.semibold};
+//       color: ${({ theme }) => theme.colors.gray[100]};
+//       background: linear-gradient(
+//         180turn,
+//         var(
+//           --title-gradient-from,
+//           ${({ theme }) => theme.colors.primary[200]}
+//         ),
+//         var(
+//           --title-gradient-to,
+//           ${({ theme }) => theme.colors.primary[500]}
+//         )
+//       );
+//       padding: 0.65rem 1rem;
+//       text-align: center;
+//     }
+//   }
+// `
 
 export default () => {
   useEffect(() => {
@@ -55,7 +55,7 @@ export default () => {
     kwesforms.init()
   })
   return (
-    <Wrapper>
+    <main>
       <form
         class="kwes-form"
         action="https://kwes.io/api/foreign/forms/RdkZ6TlyTY0ig4SwrB9s"
@@ -76,6 +76,6 @@ export default () => {
         <textarea name="message" rules="required" />
         <button type="submit">Submit</button>
       </form>
-    </Wrapper>
+    </main>
   )
 }
