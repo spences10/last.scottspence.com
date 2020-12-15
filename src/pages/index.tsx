110af5447ef62d923bde6b10cc35ea10bf370f0a
pauliescanlon <pauliescanlon@gmail.com>
2020-12-15 20:05:19 +0000

    add interface to IndexPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,38 @@
 import { Box, Heading, Link, Text } from '@chakra-ui/react'
-import { graphql, Link as GatsbyLink, PageProps } from 'gatsby'
-import React from 'react'
+import { graphql, Link as GatsbyLink } from 'gatsby'
+import React, { FunctionComponent } from 'react'
 
-export default function IndexPage({ data }: PageProps) {
+interface IndexPost {
+  id: string
+  slug: string
+  excerpt: string
+  frontmatter: {
+    date: string
+    title: string
+  }
+}
+interface IndexPageProps {
+  data: {
+    allMdx: {
+      // I've abstracted the keys within "nodes" so we can use the IndexPost interface elsewhere, the [] after IndexPost denote its an array of this shape
+      nodes: IndexPost[]
+      // You could also type it like this
+      // nodes: {
+      //   id: string
+      //   slug: string
+      //   excerpt: string
+      //   frontmatter: {
+      //     date: string
+      //     title: string
+      //   }
+      // }[]
+    }
+  }
+}
+
+// IndexPage is of type FunctionComponent which accets a generic. <T>
+// A generica can be of any shape but in your case its the shape of the data object which you define in the page query below
+const IndexPage: FunctionComponent<IndexPageProps> = ({ data }) => {
   const posts = data.allMdx.nodes
 
   return (
@@ -14,9 +44,20 @@ export default function IndexPage({ data }: PageProps) {
       <Text fontSize="xl" my={5}>
         Now go build something great.
       </Text>
-      {posts.map(post => {
+      {posts.map((post: IndexPost, index: number) => {
+        // you don't have to destructure but its my preference
+        const {
+          // id,
+          slug,
+          excerpt,
+          frontmatter: { title },
+        } = post
+
         return (
           <Box
+            // you could use id as the key and remove index from the map if you like
+            // key={id}
+            key={index}
             as="article"
             border="1px"
             borderColor="#663399"
@@ -29,10 +70,10 @@ export default function IndexPage({ data }: PageProps) {
               textDecor=""
               _hover={{ textDecor: 'underline' }}
               fontSize="xl"
-              to={post.slug}
+              to={slug}
             >
-              <Text fontSize="3xl">{post.frontmatter.title}</Text>
-              <Text fontSize="xl">{post.excerpt}</Text>
+              <Text fontSize="3xl">{title}</Text>
+              <Text fontSize="xl">{excerpt}</Text>
             </Link>
           </Box>
         )
@@ -60,6 +101,11 @@ export default function IndexPage({ data }: PageProps) {
   )
 }
 
+export default IndexPage
+
+// This shape is up to you, it has nothing to do with Gatsby or MDX. You define the query shape however you want
+// A Gatsby page query return it inside a "data" object
+// It the data object you need to type. As seen above in IndexPageProps
 export const query = graphql`
   {
     allMdx {
