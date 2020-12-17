99c2cda7895e67be58b090dbe284e615172a47eb
spences10 <spences10apps@gmail.com>
2020-12-17 18:40:32 +0000

    add default theme to dark

--- src/woot-wapper.tsx
+++ src/woot-wapper.tsx
@@ -1,8 +1,17 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Text } from '@chakra-ui/react'
+import { MDXProvider } from '@mdx-js/react'
 import { GatsbyBrowser } from 'gatsby'
 import React from 'react'
 import Layout from './components/layout'
 import { theme } from './theme'
+
+const components = {
+  h2: (props: { children: React.ReactNode }) => (
+    <Text fontSize="3xl" my={3} fontFamily="heading">
+      {props.children}
+    </Text>
+  ),
+}
 
 interface Props {
   element: GatsbyBrowser
@@ -11,7 +20,9 @@ interface Props {
 export const wrapPageElement = ({ element }: Props) => {
   return (
     <ChakraProvider theme={theme} resetCSS>
-      <Layout>{element}</Layout>
+      <MDXProvider components={components}>
+        <Layout>{element}</Layout>
+      </MDXProvider>
     </ChakraProvider>
   )
 }
