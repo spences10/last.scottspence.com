e185e7c87efdd3687cec45b635d3732c4a9fba08
spences10 <spences10apps@gmail.com>
2020-12-21 18:35:02 +0000

    p for mdx

---
+++ src/components/mdx-elements/p.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@chakra-ui/react'
+import React, { FunctionComponent, ReactNode } from 'react'
+
+export interface AProps {
+  children: ReactNode
+  href: string
+}
+
+export const P: FunctionComponent<AProps> = props => {
+  return <Text fontSize="xl">{props.children}</Text>
+}
