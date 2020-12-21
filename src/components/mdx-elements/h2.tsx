ca919a4b9d4ba0952c0f54accada182e8554a8cd
spences10 <spences10apps@gmail.com>
2020-12-21 20:17:27 +0000

    hide header link

--- src/components/mdx-elements/h2.tsx
+++ src/components/mdx-elements/h2.tsx
@@ -1,20 +1,21 @@
 import { Text } from '@chakra-ui/react'
 import React, { FunctionComponent, ReactNode } from 'react'
+import { HideLink } from './hide-link'
 
 export interface H2Props {
   children: ReactNode
 }
 
-export const H2: FunctionComponent<H2Props> = ({ children }) => {
-  // const yo = useColorModeValue('light', 'dark')
+export const H2: FunctionComponent<H2Props> = props => {
   return (
     <Text
+      {...props}
       fontSize="3xl"
       my={3}
       fontFamily="heading"
       color="brand.100"
     >
-      {children}
+      <HideLink>{props.children}</HideLink>
     </Text>
   )
 }
---
+++ src/components/mdx-elements/hide-link.tsx
@@ -0,0 +1,35 @@
+import { useColorModeValue } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+import React, { FunctionComponent, ReactNode } from 'react'
+
+export interface HideLinkProps {
+  children: ReactNode
+}
+
+export const HideLink: FunctionComponent<HideLinkProps> = props => {
+  const color = useColorModeValue(
+    '#1A202C',
+    'rgba(255, 255, 255, 0.92)'
+  )
+  const HideLinkStyle = styled.span`
+    a {
+      float: left;
+      margin-left: -25px;
+    }
+    svg {
+      visibility: hidden;
+      height: 45px;
+      width: 20px;
+    }
+    &:hover {
+      svg {
+        visibility: visible;
+        height: 45px;
+        width: 20px;
+        fill: ${color};
+      }
+    }
+  `
+
+  return <HideLinkStyle {...props}>{props.children}</HideLinkStyle>
+}
