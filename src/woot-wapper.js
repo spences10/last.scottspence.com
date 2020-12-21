ce954e0631d525dc2ef8544ee2e02fdf1c67b2b6
spences10 <spences10apps@gmail.com>
2020-12-21 18:35:28 +0000

    wrapper as js becuse no types for MDX

--- src/woot-wapper.js
+++ src/woot-wapper.js
@@ -1,7 +1,6 @@
 import { ChakraProvider, Code as InlineCode } from '@chakra-ui/react'
 import { MDXProvider } from '@mdx-js/react'
-import { GatsbyBrowser } from 'gatsby'
-import React, { ReactNode } from 'react'
+import React from 'react'
 import Layout from './components/layout'
 import {
   H1,
@@ -15,21 +14,16 @@ import {
   Tr,
   Ul,
 } from './components/mdx-elements'
+import { P } from './components/mdx-elements/p'
 import { Td } from './components/mdx-elements/td'
 import { theme } from './theme'
 
 const components = {
-  a: (
-    props: JSX.IntrinsicAttributes &
-      React.ClassAttributes<HTMLAnchorElement> &
-      React.AnchorHTMLAttributes<HTMLAnchorElement>
-  ) => <a {...props} />,
-  h1: (props: { children: ReactNode }) => <H1 {...props} />,
-  h2: (props: { children: ReactNode }) => <H2 {...props} />,
+  a: props => <a {...props} />,
+  h1: props => <H1 {...props} />,
+  h2: props => <H2 {...props} />,
   // pre: props => <Code {...props} />,
-  'p.inlineCode': (props: { children: ReactNode }) => (
-    <InlineCode {...props} />
-  ),
+  'p.inlineCode': props => <InlineCode {...props} />,
   li: props => <Li {...props} />,
   ol: props => <Ol {...props} />,
   table: props => <Table {...props} />,
@@ -39,13 +33,10 @@ const components = {
   tr: props => <Tr {...props} />,
   th: props => <Th {...props} />,
   td: props => <Td {...props} />,
+  p: props => <P {...props} />,
 }
 
-interface Props {
-  element: GatsbyBrowser
-}
-
-export const wrapPageElement = ({ element }: Props) => {
+export const wrapPageElement = ({ element }) => {
   return (
     <ChakraProvider theme={theme} resetCSS>
       <MDXProvider components={components}>
