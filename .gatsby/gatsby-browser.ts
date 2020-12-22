a624ba82da28cf1aa038c646a1bf7ba9cffea78d
spences10 <spences10apps@gmail.com>
2020-12-22 17:52:13 +0000

    bring in fontsource fonts

--- .gatsby/gatsby-browser.ts
+++ .gatsby/gatsby-browser.ts
@@ -1,3 +1,6 @@
+import 'fontsource-hepta-slab'
+import 'fontsource-work-sans'
+import 'victormono'
 import { wrapPageElement as wrap } from '../src/woot-wapper'
 
 export const wrapPageElement = wrap
