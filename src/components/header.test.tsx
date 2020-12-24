746f8f73f18923f6b184ef0234e01a042ec139a9
spences10 <spences10apps@gmail.com>
2020-12-24 13:02:13 +0000

    fix header test TODO make reusable

--- src/components/header.test.tsx
+++ src/components/header.test.tsx
@@ -1,7 +1,13 @@
 import { render } from '@testing-library/react'
 import React from 'react'
+import { ThemeProvider } from 'theme-ui'
+import theme from '../theme'
 import { Header } from './header'
 
 test('Renders the table', () => {
-  render(<Header siteTitle={`Yo`} />)
+  render(
+    <ThemeProvider theme={theme}>
+      <Header siteTitle={`Yo`} description={`a site`} />
+    </ThemeProvider>
+  )
 })
