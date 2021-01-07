61daf931747c0b30de93873e447df15ce92de189
spences10 <spences10apps@gmail.com>
2021-01-07 15:16:10 +0000

    add seo to newsletter

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import SEO from 'react-seo-component'
 import styled from 'styled-components'
 import { H1, P } from '../components/page-elements'
+import { useSiteMetadata } from '../hooks/use-site-metadata'
 
 const Wrapper = styled.main`
   min-height: 60vh;
@@ -18,33 +20,59 @@ const Wrapper = styled.main`
 `
 
 export default function MailingList() {
+  const {
+    title,
+    description,
+    siteUrl,
+    twitterUsername,
+    authorName,
+    siteLanguage,
+    siteLocale,
+  } = useSiteMetadata()
   return (
-    <Wrapper>
-      <H1>Newsletter</H1>
-      <P>
-        Occasional details about intersting things I'm working on.
-      </P>
-      <P>
-        Get involved to be in with a chance to win a dev sticker pack!
-      </P>
-      <form
-        class="kwes-form"
-        action="https://kwes.io/api/foreign/forms/j7gBZsxccB5zeDXJ6ZNY"
-      >
-        <label for="name">Your Name:</label>
-        <input
-          type="text"
-          name="name"
-          rules="required|max:255"
-        ></input>
-        <label for="email">Your Email:</label>
-        <input
-          type="text"
-          name="email"
-          rules="required|max:255"
-        ></input>
-        <button type="submit">Submit</button>
-      </form>
-    </Wrapper>
+    <>
+      <SEO
+        title={`Newsletter`}
+        titleTemplate={title}
+        description={description}
+        image={ogImageUrl(
+          authorName,
+          'scottspence.com',
+          `Newsletter - Signup`
+        )}
+        pathname={siteUrl}
+        siteLanguage={siteLanguage}
+        siteLocale={siteLocale}
+        twitterUsername={twitterUsername}
+      />
+      <Wrapper>
+        <H1>Newsletter</H1>
+        <P>
+          Occasional details about intersting things I'm working on.
+        </P>
+        <P>
+          Get involved to be in with a chance to win a dev sticker
+          pack!
+        </P>
+        <form
+          class="kwes-form"
+          action="https://kwes.io/api/foreign/forms/j7gBZsxccB5zeDXJ6ZNY"
+        >
+          <label for="name">Your Name:</label>
+          <input
+            type="text"
+            name="name"
+            rules="required|max:255"
+          ></input>
+          <label for="email">Your Email:</label>
+          <input
+            type="text"
+            name="email"
+            rules="required|max:255"
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </Wrapper>
+    </>
   )
 }
