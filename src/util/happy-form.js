48902e4b3125bdf336d77d21047c878002574c77
spences10 <spences10apps@gmail.com>
2021-01-30 16:23:06 +0000

    add helper for form automation

--- src/util/happy-form.js
+++ src/util/happy-form.js
@@ -1,16 +1,22 @@
+/**
+ * This is what is used to send the contact to SendGrid
+ * the `list_ids` is from my contacts list in SendGrid
+ * that is the id of my Newsletter list
+ */
+
 export default async function happyForm(email, name) {
   await fetch('https://api.sendgrid.com/v3/marketing/contacts', {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+      Authorization: `Bearer ${process.env.GATSBY_SENDGRID_API_KEY}`,
     },
     body: JSON.stringify({
       list_ids: ['785c6867-cc31-46e9-84af-c5bf6935acd7'],
       contacts: [
         {
           email,
-          custom_fields: { name },
+          first_name: name,
         },
       ],
     }),
