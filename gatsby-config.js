31121bcf8fcd7a9400ca6c43bd7a56af2c5bf390
spences10 <spences10apps@gmail.com>
2021-02-01 18:52:42 +0000

    update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ require('dotenv').config({
 
 const siteMetadata = {
   title: `Scott Spence`,
-  description: `A detailled account of what Scott Spence is about. 
-Posts on TypeScript, Unit testing with React testing library with Jest and end to end testing with Cypress.`,
+  description: `A detailed account of what Scott Spence is about.
+Posts on TypeScript, Unit testing, with React testing library and Jest. End to end testing with Cypress.`,
   lastBuildDate: new Date(Date.now()).toISOString(),
   siteUrl: `https://scottspence.com`,
   authorName: `Scott Spence`,
