a8308eba53629b8db304ce3358178e0a6e9d18f7
spences10 <spences10apps@gmail.com>
2021-02-10 13:24:10 +0000

    fix traced svg

--- src/components/mug-face.js
+++ src/components/mug-face.js
@@ -17,7 +17,7 @@ export const MugFace = () => {
       placeholderImage: file(relativePath: { eq: "favicon.png" }) {
         childImageSharp {
           fluid(maxWidth: 200) {
-            ...GatsbyImageSharpFluid_tracedSVG
+            ...GatsbyImageSharpFluid
           }
         }
       }
