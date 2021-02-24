2c9a531d8e9c593ec541cdc40b341d69a40b7fab
spences10 <spences10apps@gmail.com>
2021-02-24 17:50:01 +0000

    defer clarity

--- src/root-wrapper.js
+++ src/root-wrapper.js
@@ -79,7 +79,7 @@ export const wrapPageElement = ({ element }) => (
         name="monetization"
         content="$ilp.uphold.com/bzPBWkMBzLmN"
       />
-      <script type="text/javascript" src="clarity.js"></script>
+      <script type="text/javascript" src="clarity.js" defer></script>
     </Helmet>
     <AnalyticsProvider>
       <ThemeProvider theme={theme}>
