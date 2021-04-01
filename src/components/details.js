8b5f241cc3b2c29b329073c4ee00f3a4c0c85381
spences10 <spences10apps@gmail.com>
2021-04-01 11:48:18 +0100

    use react!

--- src/components/details.js
+++ src/components/details.js
@@ -1,5 +1,4 @@
-import { useState } from 'preact/hooks'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Details = ({ label, children }) => {
   const [isExpanded, setIsExpanded] = useState(false)
