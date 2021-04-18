8b5480e33802f539b88f32bf9dd2dc8506aa67dc
spences10 <spences10apps@gmail.com>
2021-04-18 12:33:56 +0100

    remove manifest as well

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,17 +123,17 @@ module.exports = {
     //   },
     // },
     `gatsby-plugin-remove-serviceworker`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: siteMetadata.title,
-        short_name: siteMetadata.title,
-        start_url: `/`,
-        background_color: `#336699`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `static/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: siteMetadata.title,
+    //     short_name: siteMetadata.title,
+    //     start_url: `/`,
+    //     background_color: `#336699`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `static/favicon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
   ],
 }
